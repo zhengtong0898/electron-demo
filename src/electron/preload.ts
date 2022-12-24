e454c6076206adfff65ed9e21b5c4f1e8f47cae4
zhengtong0898 <zhengtong0898@aliyun.com>
2022-12-24 17:57:55 +0800

    ipc: setTitle and setSize

--- src/electron/preload.ts
+++ src/electron/preload.ts
@@ -1,2 +1,18 @@
 // See the Electron documentation for details on how to use preload scripts:
 // https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
+const { contextBridge, ipcRenderer } = require('electron')
+
+
+
+contextBridge.exposeInMainWorld('electronAPI', {
+
+  setSize: (width: number, height: number) => {
+    console.log("setSize: running")
+    ipcRenderer.send('setSize', width, height)
+  },
+
+  setTitle: (title) => {
+    ipcRenderer.send('setTitle', title)
+  }
+
+})
--- src/vue/app.vue
+++ src/vue/app.vue
@@ -1,13 +1,16 @@
 <template>
     <div>Hello World!6</div>
 </template>
-  
+
   <script>
   export default {
-  
+
   }
+  console.log("Hello World!")
+  window.electronAPI.setTitle("Hello World!")
+  window.electronAPI.setSize(1440, 900)
   </script>
-  
+
   <style lang="css">
   html {
       background-color: red;
